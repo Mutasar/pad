f2d86c76993f7f3f4a9ed79259e64798b1f77377
Mutasar <49950898+Mutasar@users.noreply.github.com>
2025-03-16 13:33:23 +0700

    Add files via upload

---
+++ ds_bike_sharing.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+import streamlit as st 
+ 
+st.write(
+    """
+    # My first app
+    Hello, para calon praktisi data masa depan!
+    """
+)
